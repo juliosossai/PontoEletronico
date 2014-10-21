e569201ae2ec02a1ec019b9c0c7ed0b7f202fecd
juliosossai <juliocesar@hitechti.com.br>
2014-10-21 10:16:29 -0200

    Initial commit

---
+++ src/main/java/ponto/Inicio.java
@@ -0,0 +1,10 @@
+package ponto;
+
+/**
+ * Created by juliosossai on 21/10/2014.
+ */
+public class Inicio {
+    public static void main(String[] args){
+        System.out.println("Ola mundo");
+    }
+}
---
+++ src/main/java/ponto/modulo/Endereco.java
@@ -0,0 +1,63 @@
+package ponto.modulo;
+
+/**
+ * Created by juliosossai on 21/10/2014.
+ */
+public class Endereco {
+
+    private String logradouro;
+    private String CEP;
+    private String Numero;
+    private String Bairro;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(String numero) {
+        Numero = numero;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String bairro) {
+        Bairro = bairro;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    private String Cidade;
+    private String Estado;
+}
