e4ca664f82ce4b49f1c35fad527519e8c9da9e10
juliosossai <juliocesar@hitechti.com.br>
2014-11-05 16:58:24 -0200

    0

--- src/main/java/ponto/Inicio.java
+++ src/main/java/ponto/Inicio.java
@@ -1,10 +1,15 @@
 package ponto;
 
 /**
- * Created by juliosossai on 21/10/2014.
+ * Created by Vinicius on 21/10/2014.
  */
 public class Inicio {
-    public static void main(String[] args){
-        System.out.println("Ola mundo");
+
+    public static void main (String[] args){
+
+        System.out.println("Hello World");
+
+        //exemplo de calculo utilizando
     }
+
 }
